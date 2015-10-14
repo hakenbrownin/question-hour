89580652270121e774bc39f2fcbdad960d17688a
Leandro Tk <leandrodtk@hotmail.com>
2015-10-14 01:25:24 -0300

    Improved code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
 	var songsUrls = [
 		"https://www.youtube.com/watch?v=jofNR_WkoCE",
 		"https://www.youtube.com/watch?v=9bZkp7q19f0",
@@ -15,8 +14,6 @@ $(document).ready(function() {
 	$(".js-generate-song-button").on("click", function() {
 		var index = Math.floor(Math.random() * size);
 		var url = songsUrls[index];
-
 		window.open(url);
 	});
-
 });
